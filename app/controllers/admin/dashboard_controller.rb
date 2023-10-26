1d20f36822b8ca9fd167aef70576b48646e4f950
akinola Olanrewaju <akinola.lanrewaju@yahoo.com>
2023-10-25 22:14:26 -0700

    add dash-board counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+		# fetching the total count of products in the database
+		@total_products = Product.count()
+		@total_categories = Category.count()
   end
 end
